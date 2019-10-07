3905594c3a08c9fb5d1df9759e279d91f6cb0e48
Nanik T <nanikjava@gmail.com>
2019-10-07 13:38:29 +1100

    Fix linting issue

--- hack/boilerplate/boilerplate_test.go
+++ hack/boilerplate/boilerplate_test.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"github.com/magiconair/properties/assert"
+
 	"testing"
 )
 
 func TestPassContentThesame(t *testing.T) {
-	t1 := []string {"this is a just a string"}
-	t2  := []string {"this is a just a string"}
+	t1 := []string{"this is a just a string"}
+	t2 := []string{"this is a just a string"}
 
-	assert.Equal(t, IsContentTheSame(t1,t2),true)
+	assert.Equal(t, IsContentTheSame(t1, t2), true)
 }
 
 func TestFailContentThesame(t *testing.T) {
-	t1 := []string {"this is a just a string1"}
-	t2  := []string {"this is a just a string2"}
+	t1 := []string{"this is a just a string1"}
+	t2 := []string{"this is a just a string2"}
 
-	assert.Equal(t, !IsContentTheSame(t1,t2),true)
+	assert.Equal(t, !IsContentTheSame(t1, t2), true)
 }
